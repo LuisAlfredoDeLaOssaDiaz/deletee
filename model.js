9ccf72876bc0954caa17387be33a78f9749e5c89
Luis Alfredo <luisdelaossadiaz27@gmail.com>
2022-04-21 18:36:20 -0500

    model.js

--- model.js
+++ model.js
@@ -1,6 +1,6 @@
 record = [
     {
-        "id_recorod": "int",
+        "id_record": "int",
         "user": {
             "id": "int",
             "role": {
@@ -9,7 +9,7 @@ record = [
                 "administrative": "int"
             }
         },
-        "vehiculo": {
+        "vehicle": {
             "plate": {
                 "number": "string",
                 "captureDate": "date"
@@ -29,61 +29,3 @@ record = [
 
     }
 ]
-
-
-def addRecord():
-    print(request.data)
-    body = json.loads(request.data)
-    print(body)
-    idRecord = body["id_record"]
-    # User = body["user"]
-    UserId = body["user"]["id"]
-    # UserRole = body["user"]["role"]
-    UserRoleTeacher = body["user"]["role"]["teacher"]
-    UserRoleStudent = body["user"]["role"]["student"]
-    UserRoleAdministrative = body["user"]["role"]["administrative"]
-    # Vehicle = body["vehicle"]
-    # VehiclePlate = body["vehicle"]["plate"]
-    VehiclePlateNumber = body["vehicle"]["plate"]["number"]
-    VehiclePlateCaptuuredate = body["vehicle"]["plate"]["captureDate"]
-    #  VehicleOwoner = body["vehicle"]["owoner"]
-    VehicleOwonerTeacher = body["vehicle"]["owoner"]["teacher"]
-    VehicleOwonerStudent = body["vehicle"]["owoner"]["student"]
-    VehicleOwonerAdministrative = body["vehicle"]["owoner"]["administrative"]
-    VehicleOwonerVisitor = body["vehicle"]["owoner"]["visitor"]
-    # Status = body["status"]
-    StatusNotupdated = body["status"]["notUpdated"]
-    StatusUpdated = body["status"]["updated"]
-    Notes = body["notes"]
-
-    print(idRecord)
-    print(User)
-    print(UserId)
-    print(UserRole)
-    print(UserRoleTeacher)
-    print(UserRoleStudent)
-    print(UserRoleAdministrative)
-    print(Vehicle)
-    print(VehiclePlate)
-    print(VehiclePlateNumber)
-    print(VehiclePlateCaptuuredate)
-    print(VehicleOwoner)
-    print(VehicleOwonerTeacher)
-    print(VehicleOwonerStudent)
-    print(VehicleOwonerAdministrative)
-    print(VehicleOwonerVisitor)
-    print(Status)
-    print(StatusNotupdated)
-    print(StatusUpdated)
-    print(Notes)
-
-    userName = 1
-    print(userName)
-
-    newUser = {
-        "id_record": userName
-    }
-    # uri = "https://parcial-flask.herokuapp.com/users"
-    # response = requests.post(uri, json=newUser)
-    record.append(newUser)
-    return jsonify(newUser), 200
